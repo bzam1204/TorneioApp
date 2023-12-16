0d78c1368f525e94e6e17ee36e0290727de94723
bruno <bzam1204@gmail.com>
2023-12-16 11:06:54 -0400

    audio

--- src/app/tela-controlador-partida/botoes/Botao_24_Seg.jsx
+++ src/app/tela-controlador-partida/botoes/Botao_24_Seg.jsx
@@ -1,115 +1,172 @@
-'use client'
+"use client";
 import styled from "styled-components";
-import imagem_botao from '../../../../public/img/botoes_especiais/default_red.png'
-import imagem_botao_hover from '../../../../public/img/botoes_especiais/default_red_hovered.png'
-import imagem_botao_pressionado from '../../../../public/img/botoes_especiais/default_red_pressionado.png'
-import {useRouter} from 'next/navigation';
-import {useRecoilState} from "recoil";
-import {time_is_running} from "../../../State/time_is_running";
-import {useEffect, useState} from "react";
+import imagem_botao from "../../../../public/img/botoes_especiais/default_red.png";
+import imagem_botao_hover from "../../../../public/img/botoes_especiais/default_red_hovered.png";
+import imagem_botao_pressionado from "../../../../public/img/botoes_especiais/default_red_pressionado.png";
+import { useRouter } from "next/navigation";
+import { useRecoilState } from "recoil";
+import { time_is_running } from "../../../State/time_is_running";
+import { useEffect, useRef, useState } from "react";
 
-import '../estilo/style.css'
+import "../estilo/style.css";
 import socket from "../../../config/socket_config";
 
-
-
 const Container_Botao_24_seg = styled.div`
-  background-image: url(${props => props.img_url});
-  width: calc(100% - 4.68vw);
-  height: 7.2vw;
-  border-radius: 1.46vw;
-  display: flex;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  cursor: pointer;
-  align-items: center;
-  padding: 0 2.34vw;
-  transition: 20ms;
+    background-image: url(${(props) => props.img_url});
+    width: calc(100% - 4.68vw);
+    height: 7.2vw;
+    border-radius: 1.46vw;
+    display: flex;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    cursor: pointer;
+    align-items: center;
+    padding: 0 2.34vw;
+    transition: 20ms;
 
-  &:hover {
-    background-image: url(${props => props.img_hover_url});
-  }
+    &:hover {
+        background-image: url(${(props) => props.img_hover_url});
+    }
 
-  &:active p {
-    line-height: 4.97vw;
-  }
+    &:active p {
+        line-height: 4.97vw;
+    }
 
-  &:active {
-    background-image: url(${props => props.img_active_url});
-  }
-`
-
+    &:active {
+        background-image: url(${(props) => props.img_active_url});
+    }
+`;
 
 function handleBotaoCinza(_time_is_running) {
-    const botao = document.querySelector('#botao_24_seg')
+    const botao = document.querySelector("#botao_24_seg");
     if (!_time_is_running) {
-        botao.classList.add('botao_cinza')
+        botao.classList.add("botao_cinza");
     } else {
-        botao.classList.remove('botao_cinza')
+        botao.classList.remove("botao_cinza");
     }
 }
 
 export const Label_24_seg = styled.p`
-  font-family: Orbitron, serif;
-  width: fit-content;
-  font-size: 4.39vw;
-  font-style: normal;
-  font-weight: 700;
-  height: 4.97vw;
-  line-height: 4.46vw;
-  color: #FE5E5E;
-  user-select: none;
-`
+    font-family: Orbitron, serif;
+    width: fit-content;
+    font-size: 4.39vw;
+    font-style: normal;
+    font-weight: 700;
+    height: 4.97vw;
+    line-height: 4.46vw;
+    color: #fe5e5e;
+    user-select: none;
+`;
 
 function resetPossessionTime() {
-    socket.emit('reset');
+    socket.emit("reset");
 }
 
 function emitUpdatedInfo(setTempo) {
-    socket.emit('emitUpdatedInfo', (dados) => {
-        setTempo(dados.currentTime)
+    socket.emit("emitUpdatedInfo", (dados) => {
+        setTempo(dados.currentTime);
     });
-
-}
-
-function apita () {
-
 }
 
 export default function Botao_24_Seg() {
-    const router = useRouter()
-    const [_time_is_running, setTimeIsRunning] = useRecoilState(time_is_running)
-    const [tempo_possessao, setTempoPossessao] = useState(24)
+    const router = useRouter();
+    const [_time_is_running, setTimeIsRunning] =
+        useRecoilState(time_is_running);
+    const [tempo_possessao, setTempoPossessao] = useState(24);
+
+    const audioRef_buzzer = useRef(`audioRef_buzzer`);
+    const audioRef_apito = useRef(`audioRef_apito`);
+    const audioRef_24seg_torcida_nervosa = useRef(
+        `audioRef_24seg_torcida_nervosa`
+    );
 
     useEffect(() => {
+        const sound_24seg_torcida_nervosa = () => {
+            return {
+                play: () => {
+                    audioRef_24seg_torcida_nervosa.current.play();
+                },
 
-        socket.on('update', ({isRunning}) => {
-            setTimeIsRunning(isRunning)
-            handleBotaoCinza(isRunning)
-            if(tempo_possessao === 12) {
-                apita()
-            }
-        })
+                stop: () => {
+                    audioRef_24seg_torcida_nervosa.current.pause();
+                    audioRef_24seg_torcida_nervosa.current.currentTime = 0; // Isso irá reiniciar o áudio quando parar
+                },
+                pause: () => {
+                    audioRef_24seg_torcida_nervosa.current.pause();
+                },
+            };
+        };
+        const sound_buzzer = () => {
+            return {
+                play: () => {
+                    audioRef_buzzer.current.play();
+                },
 
-    }, [_time_is_running])
+                stop: () => {
+                    audioRef_buzzer.current.pause();
+                    audioRef_buzzer.current.currentTime = 0; // Isso irá reiniciar o áudio quando parar
+                },
+                pause: () => {
+                    audioRef_buzzer.current.pause();
+                },
+            };
+        };
+        const sound_apito = () => {
+            return {
+                play: () => {
+                    audioRef_apito.current.play();
+                },
 
-    useEffect(() => {
-        socket.on('update', ({possessionTime}) => {
-            setTempoPossessao(possessionTime)
-        })
-    }, [tempo_possessao])
+                stop: () => {
+                    audioRef_apito.current.pause();
+                    audioRef_apito.current.currentTime = 0; // Isso irá reiniciar o áudio quando parar
+                },
+                pause: () => {
+                    audioRef_apito.current.pause();
+                },
+            };
+        };
+    }, []);
 
 
-    return (<Container_Botao_24_seg
+      
+      useEffect(() => {
+        socket.on("update", ({ isRunning }) => {
+          setTimeIsRunning(isRunning);
+            handleBotaoCinza(isRunning);
+          });
+        }, [_time_is_running, setTimeIsRunning]);
 
-        onClick={() => {
-            resetPossessionTime()
-        }}
-        id={'botao_24_seg'}
-        img_url={imagem_botao.src}
-        img_hover_url={imagem_botao_hover.src}
-        img_active_url={imagem_botao_pressionado.src}>
+        useEffect(() => {
+        socket.on("update", ({ possessionTime }) => {
+            setTempoPossessao(possessionTime);
+            if (possessionTime === 22) {
+              sound_24seg_torcida_nervosa().play();
+                alert("12");
+              }
+            console.log(possessionTime === 22);
+          });
+        }, [tempo_possessao]);
+      
 
-        <Label_24_seg>{tempo_possessao}</Label_24_seg>
-    </Container_Botao_24_seg>)
+    return (
+        <Container_Botao_24_seg
+            onClick={() => {
+                resetPossessionTime();
+            }}
+            id={"botao_24_seg"}
+            img_url={imagem_botao.src}
+            img_hover_url={imagem_botao_hover.src}
+            img_active_url={imagem_botao_pressionado.src}
+        >
+            <audio
+                ref={audioRef_24seg_torcida_nervosa}
+                src="./audio/basquete_sound_24s_torcida_nervosa_cut.mp3"
+            />
+            <audio ref={audioRef_buzzer} src="./audio/buzzer.mp3" />
+            <audio ref={audioRef_apito} src="./audio/apito.mp3" />
+
+            <Label_24_seg>{tempo_possessao}</Label_24_seg>
+        </Container_Botao_24_seg>
+    );
 }
